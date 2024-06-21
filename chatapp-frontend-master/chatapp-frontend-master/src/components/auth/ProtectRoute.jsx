263803d63ebed291abea6243ae242a2a12af0a13
Dikshika <dikshikabansal461@gmail.com>
2024-06-21 17:51:45 +0530

    commit

--- chatapp-frontend-master/chatapp-frontend-master/src/components/auth/ProtectRoute.jsx
+++ chatapp-frontend-master/chatapp-frontend-master/src/components/auth/ProtectRoute.jsx
@@ -3,7 +3,7 @@ import { Navigate, Outlet } from "react-router-dom";
 
 const ProtectRoute = ({ children, user, redirect = "/login" }) => {
   if (!user) return <Navigate to={redirect} />;
-
+    
   return children ? children : <Outlet />;
 };
 
